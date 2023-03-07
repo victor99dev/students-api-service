7336a2fc9aeccc77b3966cb5af6cdaa4288244e7
Victor Hugo <victormotabatista@gmail.com>
2023-03-07 14:04:11 -0300

    docs(swagger): configuring swagger

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1.0.0",
+        Title = "Students Service.",
+        Description = "Registration flow of classes and students.",
+        Contact = new OpenApiContact
+        {
+            Name = "Victor99Dev",
+            Url = new Uri("https://victor99dev.site/")
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -23,3 +38,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
